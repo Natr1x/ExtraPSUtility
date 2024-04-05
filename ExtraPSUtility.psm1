790e47ba50f0edbf56355770170daff0f5612783
Oskar Söderbärg <oskar.soderbarg@nepartner.se>
2024-04-05 13:43:54 +0200

    Added week utility functions from getps.dev/blog

--- ExtraPSUtility.psm1
+++ ExtraPSUtility.psm1
@@ -68,3 +68,83 @@ Function Select-Expand {
         }}|Select-Object -ExcludeProperty '_tmpProp' -ExpandProperty '_tmpProp'
     }
 }
+
+
+# Get-CalendarWeek was taken (with permission) from https://getps.dev/blog/get-weekinfo/
+function Get-CalendarWeek {
+    <#
+    .DESCRIPTION
+        Gets the current week number based on a specific culture and it's week number descision rules.
+    .PARAMETER Date
+        Defines the date at which to return the week number for. Defaults to the current date.
+    .PARAMETER CultureInfo
+        Defines the culture that should be used to calculate the week number. Defaults to se-SV.
+    .EXAMPLE
+        Get-CalendarWeek
+        Get the week number for the current date.
+    .EXAMPLE
+        Get-CalendarWeek -Date 2018-01-25 -CultureInfo en-US
+        Get the week number for the date 2018-01-25 according to the week number calculation rules of the en-US culture.
+    .NOTES
+        Author: Hannes Palmquist
+        AuthorEmail: hannes.palmquist@outlook.com
+        COPYRIGHT: © 2019, Hannes Palmquist, All Rights Reserved
+    #>
+    param(
+        [datetime]$Date = (Get-Date),
+        [string]$CultureInfo = $PSCulture
+    )
+
+    # Get specific culture object
+    $Culture = [cultureinfo]::GetCultureInfo($CultureInfo)
+
+    # retrieve calendar week
+    write-output $Culture.Calendar.GetWeekOfYear($Date, $Culture.DateTimeFormat.CalendarWeekRule, $Culture.DateTimeFormat.FirstDayOfWeek)
+}
+
+# Get-WeekInfo was taken (with permission) from https://getps.dev/blog/get-weekinfo/
+function Get-WeekInfo {
+    <#
+    .DESCRIPTION
+        Gets info about a specific week
+    .PARAMETER Week
+        Defines the week number to query
+    .PARAMETER Year
+        Defines which year to query
+    .EXAMPLE
+        Get-WeekInfo -Week 5 -Year 1988
+        Gets the first date of the fifth week of 1988
+    .NOTES
+        Author: Hannes Palmquist
+        AuthorEmail: hannes.palmquist@outlook.com
+        Copyright: (c) 2019, Hannes Palmquist, All Rights Reserved
+    #>
+
+    [CmdletBinding()] # Enabled advanced function support
+    param(
+        [Parameter(Mandatory)][ValidateRange(1, 53)][int]$Week,
+        [Parameter(Mandatory)][ValidateRange(1600, 2100)][int]$Year
+    )
+
+    BEGIN {
+        $WeekHash = [ordered]@{
+            Week = $Week
+            Year = $Year
+        }
+    }
+
+    PROCESS {
+        $ReferenceDate              = Get-Date -Year $Year -Month 02 -Date 05
+        $ReferenceWeek              = Get-CalendarWeek -Date $ReferenceDate
+        $WeeksDiff                  = $Week - $ReferenceWeek
+        $DateInWeek                 = $ReferenceDate.AddDays($WeeksDiff * 7)
+        $WeekHash.FirstDateOfWeek   = $DateInWeek.AddDays(1 - [int]$DateInWeek.DayOfWeek)
+        $WeekHash.LastDateOfWeek    = $WeekHash.FirstDateOfWeek.AddDays(7).AddMilliseconds(-1)
+        $WeekHash.StartsInMonth     = ([cultureinfo]::GetCultureInfo($PSCulture)).DateTimeFormat.MonthNames[($WeekHash.FirstDateOfWeek).Month-1]
+        $WeekHash.EndsInMonth       = ([cultureinfo]::GetCultureInfo($PSCulture)).DateTimeFormat.MonthNames[($WeekHash.LastDateOfWeek).Month-1]
+    }
+
+    END {
+        Write-Output ([pscustomobject]$WeekHash)
+    }
+}
